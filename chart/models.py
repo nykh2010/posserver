ac4ab4723ba32f23ed7631cdc96168a4d5bbbcb7
nykh2010 <466721896@qq.com>
2018-09-19 19:16:36 +0800

    协议处理
    完成校时、信息更新、心跳命令的处理

--- chart/models.py
+++ chart/models.py
@@ -4,7 +4,8 @@ class PosData(models.Model):
     name = models.CharField(max_length=30)      # 姓名
     depatment = models.CharField(max_length=30)      # 科室
     level = models.CharField(max_length=30)
-    uid = models.CharField(max_length=30, primary_key=True)       # id
+    tid = models.CharField(max_length=30)       # 标签id
+    uid = models.CharField(max_length=30, primary_key=True)       # 工号
     gatewayId = models.CharField(max_length=30)     # 对应网关id
     x = models.FloatField()
     y = models.FloatField()
--- chart/views.py
+++ chart/views.py
@@ -10,14 +10,19 @@ import serial
 import numpy as np
 import threading
 
-def open_serial():
-    try:
-        s = serial.Serial(port='COM1', timeout=10)
-    except:
-        print('port already open')
-    else:
-        print('port open success')
-    return s
+def str2HexList(strObj):
+    '''字符串转十六进制列表'''
+    ret = []
+    for i, l in enumerate(strObj):
+        if i%2:
+            ret[i//2] = ret[i//2]+int(l)
+        else:
+            r = int(l) * 16
+            ret.append(r)
+
+    print(ret)
+    return ret
+
 
 def indexTest(request):
     return render(request, 'index.html')
@@ -25,6 +30,7 @@ def indexTest(request):
 def index(request):
     if request.method == 'POST':
         '''处理上传的文件信息'''
+        PosData.objects.all().delete()
         data = {}
         # 1. 提取xls文件中的字段
         for i, buff in enumerate(request.FILES['csvfile'].readlines()):
@@ -47,6 +53,7 @@ def index(request):
                     depatment = data['科室'],
                     level = data['职称'],
                     uid = data['工号'],
+                    tid = data['标签id'],
                     gatewayId = data['对应网关'],
                     x = data['x'],
                     y = data['y']
@@ -59,8 +66,8 @@ def index(request):
                 dataSet.x = data['x']
                 dataSet.y = data['y']
             dataSet.save()
-        # 3. 提取数据库文件，读取模板后返回
-        # terminals = PosData.objects.all()
+        # 3. 提取数据库文件，读取模板后返回，向网关发送信息
+
     else:
         '''处理GET请求'''
     terminals = PosData.objects.all()
@@ -68,10 +75,73 @@ def index(request):
 
 websocks = []
 
-class WebProtocol:
-    def __init__(self, jsonSubject):
-        '''网页响应信息'''
-        pass
+def WebProtocol(buff):
+    jsonDict = json.loads(buff)
+    print(jsonDict)
+    res = []
+    if jsonDict['type'] == 0x02:
+        # 定时轮询开关
+        result = [0x03,0x02,jsonDict['id'],0x01,jsonDict['message']]
+        res.append(result)
+    elif jsonDict['type'] == 0x01:
+        result = [0x03,0x01,jsonDict['id'],19]
+        tm = time.localtime()
+        tm_s = '{:4}.{:0>2}.{:0>2},{:0>2}:{:0>2}:{:0>2}'.format(
+            tm.tm_year, tm.tm_mon, tm.tm_mday, \
+            tm.tm_hour, tm.tm_min, tm.tm_sec
+        )
+        rb = bytes(tm_s, encoding='utf-8')
+        rl = list(rb)
+        result.extend(rl)
+        res.append(result)
+    elif jsonDict['type'] == 0x00:
+        # 向下发送数据
+        terminals = PosData.objects.all()
+        for terminal in terminals:
+            # 姓名
+            result = [0x03,0x03,0x01]
+            s = '{:0>4}'.format(terminal.tid)   # 字符串转换
+            l = str2HexList(s)
+            result.extend(l)            # 追加至返回列表中
+            r = terminal.name
+            rb = bytes(r, encoding='GB2312')
+            length = len(rb)
+            rl = list(rb)
+            result.extend(rl)
+            result.insert(3, length+2)
+            res.append(result)
+            # 科室和职级
+            result = [0x03,0x04,0x01]
+            s = '{:0>4}'.format(terminal.tid)   # 字符串转换
+            l = str2HexList(s)
+            result.extend(l)            # 追加至返回列表中
+            r = '%s %s' % (terminal.depatment, terminal.level)
+            rb = bytes(r, encoding='GB2312')
+            length = len(rb)
+            rl = list(rb)
+            result.extend(rl)
+            result.insert(3, length+2)
+            res.append(result)
+            # 工牌号
+            result = [0x03,0x05,0x01]
+            s = '{:0>4}'.format(terminal.tid)   # 字符串转换
+            l = str2HexList(s)
+            result.extend(l)            # 追加至返回列表中
+            result.insert(6, 0x04)
+            r = '{:0>14}'.format(terminal.uid)
+            rb = bytes(r, encoding='utf-8')
+            length = len(rb)
+            rl = list(rb)
+            result.extend(rl)
+            result.insert(3, length+2)
+            res.append(result)
+            
+    for r in res:
+        check = 0       # 校验位
+        for x in r:
+            check = check ^ x
+        r.append(check)
+    return res
 
 @accept_websocket
 def websockTest(request):
@@ -83,10 +153,19 @@ def websockTest(request):
         websock = request.websocket
         websocks.append(websock)
         print(len(websocks))
+        
         for message in websock:
-            print(message)
-            ms.write(message)
+            if message is None:
+                print('message is none')
+                websocks.remove(websock)
+                break
+            result = WebProtocol(message)
+            for msgdata in result:
+                print(msgdata)
+                ms.write(msgdata)
+                # 等待应答
             websock.send(message)
+        return
 
 class MySerial(serial.Serial):
     def __init__(self, port='COM10'):
@@ -128,6 +207,9 @@ class MySerial(serial.Serial):
         elif buff[0] == 0x02 and buff[1] == 0x08:
             '''定位信息上传'''
             result = Result(True, str(buff[4:6]), buff[1], str(buff[6:10]))
+        elif buff[0] == 0x02 and buff[1] == 0x00:
+            '''网关上线'''
+            result = Result(False, str(buff[1]), 0x00)
         else:
             '''网关应答'''
             result = Result(False)
@@ -136,47 +218,4 @@ class MySerial(serial.Serial):
 
 
 ms = MySerial()
-
-        # try:
-        #     s = serial.Serial(port='COM1', timeout=1)
-        # except:
-        #     print('port already open')
-        # else:
-        #     print('port open success')
-        # websock = request.websocket
-        # position = {}
-        # pos = PosData.objects.all()
-        # # help(pos)
-        # for p in pos:
-        #     position['x'] = p.x
-        #     position['y'] = p.y
-        #     position['value'] = p.uid
-        #     jsonstr = json.dumps(position)
-        #     websock.send(jsonstr.encode('utf-8'))
-        # # print(posData)
-        # while True:
-        #     buff = s.read(2048)
-        #     if not buff:
-        #         continue
-        #     print(buff)
-        #     position['x'] = buff[1]
-        #     position['y'] = buff[2]
-        #     position['value'] = buff[0]
-        #     try:
-        #         p = PosData.objects.get(uid=position['value'])
-        #     except:
-        #         # 没有记录，新增一条
-        #         p = PosData(uid=position['value'], x=position['x'], y=position['y'])
-        #     else:
-        #         # 有记录
-        #         p.x = position['x']
-        #         p.y = position['y']
-        #     p.save()
-                
-        #     # position['y'] = random.randint(1,10)
-        #     # print(position)
-        #     jsonstr = json.dumps(position)
-        #     # print(jsonstr)
-        #     # 发送位置信息
-        #     websock.send(jsonstr.encode('utf-8'))
 
