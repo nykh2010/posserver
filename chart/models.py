52e591bf931d16badcc347a7565ab4d5f6013654
nykh2010 <466721896@qq.com>
2018-09-23 22:41:07 +0800

    协议增加
    新增时间校准，信息下发

--- chart/models.py
+++ chart/models.py
@@ -3,9 +3,10 @@ from django.db import models
 class PosData(models.Model):
     name = models.CharField(max_length=30)      # 姓名
     depatment = models.CharField(max_length=30)      # 科室
-    level = models.CharField(max_length=30)
+    level = models.CharField(max_length=30)     # 职级
     tid = models.CharField(max_length=30)       # 标签id
     uid = models.CharField(max_length=30, primary_key=True)       # 工号
     gatewayId = models.CharField(max_length=30)     # 对应网关id
-    x = models.FloatField()
-    y = models.FloatField()
+    onlineFlag = models.BooleanField(default=False)          # 在线标志
+    x = models.FloatField(default=0)
+    y = models.FloatField(default=0)
--- chart/views.py
+++ chart/views.py
@@ -10,6 +10,7 @@ import serial
 import numpy as np
 import threading
 import os
+import queue
 
 
 cwd = os.getcwd()
@@ -61,6 +62,7 @@ def index(request):
                     uid = data['工号'],
                     tid = data['标签id'],
                     gatewayId = data['对应网关'],
+                    onlineFlag = True,
                     x = data['x'],
                     y = data['y']
                 )
@@ -91,6 +93,7 @@ def WebProtocol(buff):
         result = [0x03,0x02,jsonDict['id'],0x01,jsonDict['message']]
         res.append(result)
     elif jsonDict['type'] == 0x01:
+        # 时间同步
         result = [0x03,0x01,jsonDict['id'],19]
         tm = time.localtime()
         tm_s = '{:4}.{:0>2}.{:0>2},{:0>2}:{:0>2}:{:0>2}'.format(
@@ -101,9 +104,59 @@ def WebProtocol(buff):
         rl = list(rb)
         result.extend(rl)
         res.append(result)
-    elif jsonDict['type'] == 0x00:
+    elif jsonDict['type'] == 0x03:
+        # 个人信息更新
+        try:
+            terminal = PosData.objects.get(uid=jsonDict['id'])
+        except:
+            print("no data set")
+            return
+        terminal.name = jsonDict['message']['name']
+        terminal.depatment = jsonDict['message']['depart']
+        terminal.level = jsonDict['message']['level']
+        terminal.uid = jsonDict['message']['uid']
+        terminal.save()
+        # 个人信息组帧
+        # 姓名
+        result = [0x03,0x03,0x01]
+        s = '{:0>4}'.format(terminal.tid)   # 字符串转换
+        l = str2HexList(s)
+        result.extend(l)            # 追加至返回列表中
+        r = terminal.name
+        rb = bytes(r, encoding='GB2312')
+        length = len(rb)
+        rl = list(rb)
+        result.extend(rl)
+        result.insert(3, length+2)
+        res.append(result)
+        # 科室和职级
+        result = [0x03,0x04,0x01]
+        s = '{:0>4}'.format(terminal.tid)   # 字符串转换
+        l = str2HexList(s)
+        result.extend(l)            # 追加至返回列表中
+        r = '%s %s' % (terminal.depatment, terminal.level)
+        rb = bytes(r, encoding='GB2312')
+        length = len(rb)
+        rl = list(rb)
+        result.extend(rl)
+        result.insert(3, length+2)
+        res.append(result)
+        # 工牌号
+        result = [0x03,0x05,0x01]
+        s = '{:0>4}'.format(terminal.tid)   # 字符串转换
+        l = str2HexList(s)
+        result.extend(l)            # 追加至返回列表中
+        result.insert(6, 0x04)
+        r = '{:0>14}'.format(terminal.uid)
+        rb = bytes(r, encoding='utf-8')
+        length = len(rb)
+        rl = list(rb)
+        result.extend(rl)
+        result.insert(3, length+2)
+        res.append(result)
+    elif jsonDict['type'] == 0x04:
         # 向下发送数据
-        terminals = PosData.objects.all()
+        terminals = PosData.objects.all()       # 获取所有数据集
         for terminal in terminals:
             # 姓名
             result = [0x03,0x03,0x01]
@@ -142,7 +195,26 @@ def WebProtocol(buff):
             result.extend(rl)
             result.insert(3, length+2)
             res.append(result)
-            
+    elif jsonDict['type'] == 0x06:
+        # 消息推送
+        try:
+            terminal = PosData.objects.get(uid=jsonDict['id'])
+        except:
+            print("no data set")
+            return
+        result = [0x03,0x06,0x01]
+        s = '{:0>4}'.format(terminal.tid)   # 字符串转换
+        l = str2HexList(s)
+        result.extend(l)
+        r = jsonDict['message']
+        rb = bytes(r, encoding='GB2312')
+        length = len(rb)
+        rl = list(rb)
+        result.extend(rl)
+        result.insert(3, length+2)
+        res.append(result)
+
+
     for r in res:
         check = 0       # 校验位
         for x in r:
@@ -169,25 +241,46 @@ def websockTest(request):
             result = WebProtocol(message)
             for msgdata in result:
                 print(msgdata)
-                ms.write(msgdata)
+                ms.sendQueue.put_nowait(msgdata)
                 # 等待应答
             websock.send(message)
         return
 
+class Result:
+    def __init__(self, needToWeb, id=None, msgType=None, msg=None):
+        self.needToWeb = needToWeb
+        self.message = {'id':id, 'type':msgType, 'message':msg}
+
 class MySerial(serial.Serial):
-    def __init__(self, port='COM10'):
+    def __init__(self, port='COM1'):
         print('myserial init')
         super().__init__(port=port, timeout=1)
+        self.sendQueue = queue.Queue(maxsize=50)
+        self.recvQueue = queue.Queue(maxsize=50)
         t = threading.Thread(target=self.loop, name='serail_loop')
         t.start()
+    
+    def checkAck(self, result):
+        '''检查应答'''
+        return True
 
     def loop(self):
         while True:
+            # 发送队列优先级高于接收队列
+            while not self.sendQueue.empty():
+                '''发送队列非空'''
+                print('serial loop')
+                msg = self.sendQueue.get_nowait()   # 从队列中取出一个消息
+                for i in range(3):
+                    self.write(msg)
+                    if self.checkAck(msg):          # 收到应答，跳出
+                        break
+            '''处理上报数据'''
             buff = self.read(2048)
             if not buff:
                 continue
             print(buff)
-            result = self.protocol(buff)
+            result = self.rxProtocol(buff)
             if result.needToWeb:
                 '''需要在页面上更新，转json格式'''
                 message = json.dumps(result.message)
@@ -200,13 +293,13 @@ class MySerial(serial.Serial):
                     check = check ^ x
                 resp.append(check)
                 self.write(resp)
+
+    def txProtocol(self, msg):
+        '''发送协议处理'''
+        pass
     
-    def protocol(self, buff):
+    def rxProtocol(self, buff):
         '''协议处理'''
-        class Result:
-            def __init__(self, needToWeb, id=None, msgType=None, msg=None):
-                self.needToWeb = needToWeb
-                self.message = {'id':id, 'type':msgType, 'message':msg}
         
         if buff[0] == 0x02 and buff[1] == 0x07:
             '''报警信息上报'''
